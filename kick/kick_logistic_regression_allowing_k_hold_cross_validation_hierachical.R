fdba7fbf7511427e9d0e4b789acebc01078d7d9f
KamonohashiPerry <economics.teru@gmail.com>
2019-08-17 18:42:04 +0900

    For Study

--- kick/kick_logistic_regression_allowing_k_hold_cross_validation_hierachical.R
+++ kick/kick_logistic_regression_allowing_k_hold_cross_validation_hierachical.R
@@ -2,7 +2,6 @@ library(tidyverse)
 library(rstan)
 library(GGally)
 library(shinystan)
-library(loo)
 library(pbmcapply)
 
 rstan_options(auto_write = TRUE)
@@ -16,7 +15,7 @@ ggpairs(wine_dataset)
 
 # Making group -----------------------------------------------------------
 wine_dataset <- wine_dataset %>% mutate(citric_acid_group = if_else(citric.acid < 0.2, 1,
-                                                    if_else(citric.acid < 0.4, 2, 3)))
+                                                                    if_else(citric.acid < 0.4, 2, 3)))
 
 wine_dataset <- wine_dataset %>% select(-citric.acid)
 
@@ -41,6 +40,12 @@ y_test <- y[-train_ind]
 x_train <- x[train_ind,]
 x_test <- x[-train_ind,]
 
+x_train_group <- x_train[,ncol(x_train)]
+x_test_group <- x_test[,ncol(x_test)]
+
+x_train <- x_train[,1:(ncol(x_train)-1)]
+x_test <- x_test[,1:(ncol(x_test)-1)]
+
 x_train <- scale(x_train)
 x_test <- scale(x_test)
 
@@ -52,23 +57,26 @@ K <- ncol(x_train) #n umber of predictors
 # Create cross validation data --------------------------------------------
 # create 10 folds of data
 hh <- kfold_split_random(n_fold, N) #hh index the fold ID of each data point
-holdout_10 <- matrix(0, nrow = N, ncol = n_fold)
-for(i in 1:N) holdout_10[i, hh[i]] <- 1
+holdout_k <- matrix(0, nrow = N, ncol = n_fold)
+for(i in 1:N) holdout_k[i, hh[i]] <- 1
 
 # turn into a list
-holdout_10 <- split(holdout_10,rep(1:ncol(holdout_10),each=nrow(holdout_10)))
+holdout_k <- split(holdout_k,rep(1:ncol(holdout_k),each=nrow(holdout_k)))
 
 
 # the basic data object
 data_m <- list(N=N,
                K=K,
+               M = length(unique(x_train_group)),
+               citric_acid_group = x_train_group,
                X=x_train,
-               y=y_train)
+               y=y_train
+               )
 
 # create a list of data list
-data_l <- rep(list(data_m),10)
+data_l <- rep(list(data_m),n_fold)
 # add the holdout index to it
-for(i in 1:10) data_l[[i]]$holdout <- holdout_10[[i]]
+for(i in 1:n_fold) data_l[[i]]$holdout <- holdout_k[[i]]
 
 
 # Define function ---------------------------------------------------------
@@ -109,10 +117,6 @@ ss <- stan_kfold(file="model/logistic_regression_allowing_k_fold_cross_validatio
                  chains=4,
                  cores=2)
 
-ss_normal <- stan_kfold(file="model/logistic_regression_allowing_k_fold_cross_validation.stan",
-                 data_l,
-                 chains=4,
-                 cores=2)
 
 
 # Calculate Mean AUC ------------------------------------------------------
@@ -121,14 +125,18 @@ set.seed(123)
 cv_mean_auc <- NULL
 for (i in 1:n_fold){
   ext_fit <- extract(ss[[i]]) # choose 1 chunk
-  coef_fit_1 <- mean(ext_fit$alpha)
-  coef_fit_2 <- colMeans(ext_fit$beta)
   
-  lin_comb <- data_l[[i]]$X[data_l[[i]]$holdout > 0, ] %*% coef_fit_2 
-                                + replicate(sum(data_l[[i]]$holdout), 1) * coef_fit_1
+  # グループによって推定したパラメータが違う
+  coef_list <- NULL
+  group_list <- data_l[[i]]$citric_acid_group[data_l[[i]]$holdout > 0]
+  for(j in 1:length(group_list)){
+    coef_list <- rbind(coef_list, colMeans(ext_fit$beta[,group_list[j],]))
+  }
+  
+  lin_comb <- rowSums(data_l[[i]]$X[data_l[[i]]$holdout > 0, ] * coef_list)
   prob <- 1/(1 + exp(-lin_comb))
   pred_value <- rbinom(sum(data_l[[i]]$holdout), 1, prob)
-
+  
   # Syntax (response, predictor):
   auc = pROC::auc(data_l[[i]]$y[data_l[[i]]$holdout > 0], pred_value)[1]
   cv_mean_auc <- append(cv_mean_auc, auc)
@@ -138,39 +146,24 @@ cv_mean_auc
 mean(cv_mean_auc)
 
 
-## no hierachical model
-set.seed(123)
-cv_mean_auc_normal <- NULL
-for (i in 1:n_fold){
-  ext_fit <- extract(ss_normal[[i]]) # choose 1 chunk
-  coef_fit_1 <- mean(ext_fit$alpha)
-  coef_fit_2 <- colMeans(ext_fit$beta)
-  
-  lin_comb <- data_l[[i]]$X[data_l[[i]]$holdout > 0, ] %*% coef_fit_2 
-  + replicate(sum(data_l[[i]]$holdout), 1) * coef_fit_1
-  prob <- 1/(1 + exp(-lin_comb))
-  pred_value <- rbinom(sum(data_l[[i]]$holdout), 1, prob)
-  
-  # Syntax (response, predictor):
-  auc = pROC::auc(data_l[[i]]$y[data_l[[i]]$holdout > 0], pred_value)[1]
-  cv_mean_auc_normal <- append(cv_mean_auc_normal, auc)
-}
-
-cv_mean_auc_normal
-mean(cv_mean_auc_normal)
-
 
 # Prediction --------------------------------------------------------------
 # Choose best model in cross-validation
 ext_fit <- extract(ss[[1]]) # choose 1 chunk
 
-# Extract posteriod distributions
-alpha_post <- ext_fit$alpha
-beta_post <- ext_fit$beta
+# グループによって推定したパラメータが違う
+beta_post <- NULL
+group_list <- x_test_group
 
-lin_comb <- replicate(nrow(x_test), 1)*mean(alpha_post) + x_test %*% colMeans(ext_fit$beta) 
+for(j in 1:length(group_list)){
+  beta_post <- rbind(beta_post, colMeans(ext_fit$beta[,group_list[j],]))
+}
+
+lin_comb <- rowSums(x_test * beta_post)
 prob <- 1/(1 + exp(-lin_comb))
 pred_value <- rbinom(nrow(x_test), 1, prob)
+
+
 # Syntax (response, predictor):
 auc = pROC::auc(y_test, pred_value)[1]
 auc
--- model/logistic_regression_allowing_k_fold_cross_validation_hierachical.stan
+++ model/logistic_regression_allowing_k_fold_cross_validation_hierachical.stan
@@ -1,49 +1,31 @@
 data {
   int<lower=0> N;   // number of data items
   int<lower=0> K;   // number of predictors
-  matrix[N, K] X;
+  int<lower=0> M;   // number of group
+  row_vector[K] X[N];
   int<lower=0,upper=1> y[N];
-  
+  int<lower=0,upper=M> citric_acid_group[N];
   int<lower=0, upper=1> holdout[N];
   // index whether the observation should be held out (1) or used (0)
 }
 
 parameters {
-  real alpha;
-  vector[K] beta;       // coefficients for predictors
-  real mu_alpha; // the mean intercept
-  real mu_beta; // the mean slope
-
-  real<lower=0> sigmasq_y;
-  real<lower=0> sigmasq_alpha;
-  real<lower=0> sigmasq_beta;
-}
-
-transformed parameters {
-  real<lower=0> sigma_alpha; // sd of intercept distribution
-  real<lower=0> sigma_beta; // sd of slope distribution
-
-  sigma_alpha = sqrt(sigmasq_alpha);
-  sigma_beta = sqrt(sigmasq_beta);
+  real mu[K];
+  real<lower=0> sigma[K];
+  vector[K] beta[M];
 }
 
 model {
-  vector[N] mu; // the linear predictor
-  mu = X * beta; // the regresion
-  // priors
-  mu_alpha ~ normal(0, 100); // non-informative prior
-  mu_beta ~ normal(0, 100); // non-informative prior
-  sigmasq_y ~ inv_gamma(0.001, 0.001); // conjugate prior of normal
-  sigmasq_alpha ~ inv_gamma(0.001, 0.001); // conjugate prior of normal
-  sigmasq_beta ~ inv_gamma(0.001, 0.001); // conjugate prior of normal
-  alpha ~ normal(mu_alpha, sigma_alpha); // all intercepts are normal 
-  
-  beta[1:K] ~ normal(mu_beta, sigma_beta);
+  for (k in 1:K) {
+    mu[k] ~ normal(0, 100);
+    sigma[k] ~ inv_gamma(1, 1);
 
-  // likelihood holding out some data
-  for(n in 1:N){
-    if(holdout[n] == 0){
-      target += bernoulli_lpmf( y[n] | inv_logit(alpha + mu[n]));
-    }
+    for (m in 1:M)
+      beta[m,k] ~ normal(mu[k], sigma[k]);
   }
+  for (n in 1:N){
+    if(holdout[n] == 0){
+      target += bernoulli_lpmf( y[n] | inv_logit(X[n] * beta[citric_acid_group[n]]));
+  }
+ }
 }
